1e417e596fb360cacc1171e906f20667aac1e492
Jheimys Bassani <jheimysbb@gmail.com>
2022-08-05 17:06:57 -0300

    atualizacao

--- frontend/src/components/pages/User/Profile.js
+++ frontend/src/components/pages/User/Profile.js
@@ -1,4 +1,145 @@
 import api from '../../../utils/api'
+
+import Input from '../../form/Input'
+
+import { useState, useEffect } from 'react'
+
+import styles from './Profile.module.css'
+import formStyles from '../../form/Form.module.css'
+
+/* hooks */
+import useFlashMessage from '../../../hooks/useFlashMessage'
+import RoundedImage from '../../layout/RoundedImage'
+
+function Profile() {
+  const [user, setUser] = useState({})
+  const [preview, setPreview] = useState()
+  const [token] = useState(localStorage.getItem('token') || '')
+  const { setFlashMessage } = useFlashMessage()
+
+  useEffect(() => {
+    api
+      .get('/users/checkuser', {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+        },
+      })
+      .then((response) => {
+        setUser(response.data)
+      })
+  }, [token])
+
+  function handleChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.value })
+  }
+
+  function onFileChange(e) {
+    setPreview(e.target.files[0])
+    setUser({ ...user, [e.target.name]: e.target.files[0] })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    let msgType = 'success'
+
+    const formData = new FormData()
+
+    const userFormData = await Object.keys(user).forEach((key) =>
+      formData.append(key, user[key]),
+    )
+
+    formData.append('user', userFormData)
+
+    const data = await api
+      .patch(`/users/edit/${user._id}`, formData, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then((response) => {
+        console.log(response.data)
+        return response.data
+      })
+      .catch((err) => {
+        console.log(err)
+        msgType = 'error'
+        return err.response.data
+      })
+
+    setFlashMessage(data.message, msgType)
+  }
+
+  return (
+    <section>
+      <div className={styles.profile_header}>
+        <h1>Perfil</h1>
+        {(user.image || preview) && (
+          <RoundedImage
+            src={
+              preview
+                ? URL.createObjectURL(preview)
+                : `${process.env.REACT_APP_API}/images/users/${user.image}` 
+            }
+            alt={user.name}
+          />
+        )}
+      </div>
+      <form onSubmit={handleSubmit} className={formStyles.form_container}>
+        <Input
+          text="Imagem"
+          type="file"
+          name="image"
+          handleOnChange={onFileChange}
+        />
+        <Input
+          text="E-mail"
+          type="email"
+          name="email"
+          placeholder="Digite o e-mail"
+          handleOnChange={handleChange}
+          value={user.email || ''}
+        />
+        <Input
+          text="Nome"
+          type="text"
+          name="name"
+          placeholder="Digite o nome"
+          handleOnChange={handleChange}
+          value={user.name || ''}
+        />
+        <Input
+          text="Telefone"
+          type="text"
+          name="phone"
+          placeholder="Digite o seu telefone"
+          handleOnChange={handleChange}
+          value={user.phone || ''}
+        />
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite a sua senha"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Confirmação de senha"
+          type="password"
+          name="confirmpassword"
+          placeholder="Confirme a sua senha"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="Editar" />
+      </form>
+    </section>
+  )
+}
+
+export default Profile
+
+/* import api from '../../../utils/api'
 import { useState, useEffect } from 'react'
 
 import styles from './Profile.module.css'
@@ -7,11 +148,12 @@ import formStyles from '../../form/Form.module.css'
 import Input from '../../form/Input'
 //import { response } from 'express'
 import useFlashMessage from '../../../hooks/useFlashMessage'
+import RoundedImage from '../../layout/RoundedImage'
 
 function Profile(){
 
     const [user, setUser] = useState({})
-    const [preview, setpreview] = useState('')
+    const [preview, setPreview] = useState()
     const [token] = useState(localStorage.getItem('token') || '')
     const {setFlashMessage} = useFlashMessage()
 
@@ -27,7 +169,7 @@ function Profile(){
 
     function onFileChange(e){
 
-        setpreview(e.target.files[0])
+        setPreview(e.target.files[0])
         setUser({ ...user, [e.target.name]: e.target.files[0]})
 
     }
@@ -48,8 +190,8 @@ function Profile(){
 
         const data  = await api.patch(`/users/edit/${user._id}`, formData, {
             headers: {
-                Authorization: `Beare ${JSON.parse(token)}`,
-                'content-type': 'multipart/form-data'
+                Authorization: `Bearer ${JSON.parse(token)}`,
+                'Content-type': 'multipart/form-data'
             }
         }).then((response) => {
 
@@ -69,7 +211,7 @@ function Profile(){
             <div className={styles.profile_header}>
                 <h1>Perfil</h1>
                 {(user.image || preview) && (
-                    <img 
+                    <RoundedImage
                         src={
                             preview 
                                 ? URL.createObjectURL(preview) 
@@ -82,7 +224,7 @@ function Profile(){
 
             <form onSubmit={handleSubmit} className={formStyles.form_container}>
                 <Input 
-                    text='Image'
+                    text='Imagem'
                     type='file'
                     name='image'
                     handleOnChange={onFileChange}
@@ -138,4 +280,4 @@ function Profile(){
     )
 }
 
-export default Profile
+export default Profile */
