96ed9743e50e7a1ac1d4a54752df290b4113286e
Jheimys Bassani <jheimysbb@gmail.com>
2022-08-09 16:15:38 -0300

    criando form de pets

--- frontend/src/components/pages/pets/AddPet.js
+++ frontend/src/components/pages/pets/AddPet.js
@@ -3,6 +3,7 @@ import styles from './AddPet.module.css'
 import { useState } from 'react'
 
 import { useNavigate } from 'react-router-dom'
+import PetForm from '../../form/PetForm'
 
 /* hooks */
 import useFlashMessage from '../../../hooks/useFlashMessage'
@@ -15,7 +16,7 @@ function AddPet(){
                 <p>Depois ele ficará disponível para adoção</p>
             </div>
             
-            <p>formulário</p>
+            <PetForm btnText='Cadastrar Pet'/>
         </section>
     )
 }
