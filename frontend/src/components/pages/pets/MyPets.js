9ee7a67ab8451d7a8036bced83985fd4cad4c5b7
Jheimys Bassani <jheimysbb@gmail.com>
2022-08-10 16:18:15 -0300

    Requisicao para pets cadastrados

--- frontend/src/components/pages/pets/MyPets.js
+++ frontend/src/components/pages/pets/MyPets.js
@@ -1,10 +1,33 @@
+import api from '../../../utils/api'
 import { useState, useEffect } from 'react'
 
 import { Link } from 'react-router-dom'
 
+import RoundedImage from '../../layout/RoundedImage'
+
+/* hooks */
+import useFlashMessage from '../../../hooks/useFlashMessage'
+
+//            ATENÇÃO!!
+//import { application } from 'express'--> foi importado de maneira automatica
+//gera problema.
+
 function MyPets(){
 
     const [pets, setPets] = useState([])
+    const [token] = useState(localStorage.getItem('token') || '')
+    const {setFlashMessage} = useFlashMessage()
+
+    useEffect(() => {
+        api.get('/pets/mypets',{
+            headers: {
+                Authorization: `Bearer ${JSON.parse(token)}`
+            }
+        })
+        .then((response) => {
+            setPets(response.data.pets)
+        })
+    },[token])
 
     return(
         <section>
