a914f391d01b1caf091135b7c5d3ef8235fee4a6
Jheimys Bassani <jheimysbb@gmail.com>
2022-08-10 14:49:14 -0300

    Conclusao da tela de add pet

--- frontend/src/components/form/PetForm.js
+++ frontend/src/components/form/PetForm.js
@@ -29,7 +29,7 @@ function PetForm({ handleSubmit, petData, btnText }){
     function submit(e){
         e.preventDefault()
         console.log(pet)
-        //handleSubmit(pet)
+        handleSubmit(pet)
     }
     
     return (
--- frontend/src/components/pages/pets/AddPet.js
+++ frontend/src/components/pages/pets/AddPet.js
@@ -2,6 +2,7 @@ import api from '../../../utils/api'
 import styles from './AddPet.module.css'
 import { useState } from 'react'
 
+//import { useHistory } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import PetForm from '../../form/PetForm'
 
@@ -9,6 +10,52 @@ import PetForm from '../../form/PetForm'
 import useFlashMessage from '../../../hooks/useFlashMessage'
 
 function AddPet(){
+    const [token] = useState(localStorage.getItem('token') || '')
+    const {setFlashMessage} = useFlashMessage()
+    //const history = useHistory()
+    const navigate = useNavigate()
+
+    async function registerPet(pet){
+        let msgType = 'success'
+
+        const formData = new FormData()
+
+        await Object.keys(pet).forEach((key) => {
+            if(key === 'images') {
+                for(let i = 0; i < pet[key].length; i++ ){
+                    formData.append('images', pet[key][i])   
+                }
+            } else {
+               formData.append(key, pet[key]) 
+            }
+        })
+
+        const data = await api
+        .post(`pets/create`, formData, {
+          headers: {
+            Authorization: `Bearer ${JSON.parse(token)}`,
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+        .then((response) => {
+          console.log(response.data)
+          return response.data
+        })
+        .catch((err) => {
+          console.log(err)
+          msgType = 'error'
+          return err.response.data
+        })
+
+        setFlashMessage(data.message, msgType)
+
+        if(msgType !== 'error'){
+            navigate.push('pets/create')
+        }
+
+
+    }
+
     return(
         <section className={styles.addpet_header}>
             <div>
@@ -16,7 +63,7 @@ function AddPet(){
                 <p>Depois ele ficará disponível para adoção</p>
             </div>
             
-            <PetForm btnText='Cadastrar Pet'/>
+            <PetForm handleSubmit={registerPet} btnText='Cadastrar Pet'/>
         </section>
     )
 }
