5d18c86d721ac54b5de0c1abaa3dfce04e8307e6
Jheimys Bassani <jheimysbb@gmail.com>
2022-08-11 15:23:44 -0300

    removendo pet

--- frontend/src/components/pages/pets/MyPets.js
+++ frontend/src/components/pages/pets/MyPets.js
@@ -30,6 +30,26 @@ function MyPets(){
         })
     },[token])
 
+    async function removePet(id){
+        let msgType = 'success'
+
+        const data = await api.delete(`/pets/${id}`, {
+            headers:{
+                Authorization:`Bearer ${JSON.parse(token)}`
+            }
+        }).then((response) => {
+            const updatedPets = pets.filter((pet) => pet._id != id)
+            setPets(updatedPets)
+            return response.data
+        })
+        .catch((err) => {
+            msgType = 'error'
+            return err.response.data
+        })
+
+        setFlashMessage(data.message, msgType)
+    }
+
     return(
         <section >
 
@@ -60,7 +80,9 @@ function MyPets(){
                                         )}
 
                                         <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
-                                        <button >Excluir</button>
+                                        <button onClick={() => {
+                                            removePet(pet._id)
+                                        }}>Excluir</button>
                                     </>
                                 ) : <p>Pet já adotado</p>}
                             </div>
