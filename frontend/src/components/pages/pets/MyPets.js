ba93d20a4e51807dc68c819966ac1f88f96f1668
Jheimys Bassani <jheimysbb@gmail.com>
2022-08-11 15:06:46 -0300

    css de pet

--- frontend/src/components/pages/pets/MyPets.js
+++ frontend/src/components/pages/pets/MyPets.js
@@ -31,35 +31,39 @@ function MyPets(){
     },[token])
 
     return(
-        <section>
+        <section >
 
-            <div>
+            <div className={styles.petlist_header}>
                 <h1>MyPets</h1>
                 <Link to="/pet/add">Cadastrar Pets</Link>
             </div>
 
-            <div>
+            <div className={styles.petlist_container}>
                 {pets.length > 0 && 
                     pets.map((pet) => (
-                        <div key={pet._id}>
+                        <div key={pet._id} className={styles.petlist_row}>
                             <RoundedImage 
                                 src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`}
                                 alt={pet.name}
-                                width='75px'
+                                width='px75'
                             />
                             <span className='bold'>{pet.name}</span>
-                            <div className={styles.action}></div>
-                            {pet.available ?
-                            (
-                                <>
-                                    {pet.adopter && (
-                                        <button>Concluir adoção</button>
-                                    )}
 
-                                    <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
-                                    <button>Excluir</button>
-                                </>
-                            ) : <p>Pet já adotado</p>}
+                            <div className={styles.actions}>
+                                {pet.available ?
+                                (
+                                    <>
+                                        {pet.adopter && (
+                                            <button className={styles.conclude_btn}>
+                                                Concluir adoção
+                                            </button>
+                                        )}
+
+                                        <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
+                                        <button >Excluir</button>
+                                    </>
+                                ) : <p>Pet já adotado</p>}
+                            </div>
                         </div>
                     ))
                 }
