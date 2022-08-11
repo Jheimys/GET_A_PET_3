7fe76b9092fe36095ba7de38c1578f579a404d23
Jheimys Bassani <jheimysbb@gmail.com>
2022-08-11 18:23:09 -0300

    Exibindo pet na home

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -20,7 +20,33 @@ function Home(){
 
     return(
         <section>
-            <h1>Home</h1>
+
+            <div>
+                <h1>Adote um Pet</h1>
+                <p>Veja o detalhes de cada um  e conheça o tutor deles</p>
+            </div>
+
+            <div>
+                {pets.length > 0 && 
+                    pets.map((pet) => (
+                        <div>
+                            <p>Imagem do pet</p>
+                            <h3>{pet.name}</h3>
+                            <p>
+                                <span className="bold">Peso:</span> {pet.weight}Kg
+                            </p>
+
+                            {pet.available ? (
+                                <Link to={`pets/${pet._id}`}><p>Mais detalhes</p></Link>) 
+                            :( <p>Adotado</p>)}
+                        </div>
+                    ))
+                }
+                {pets.length === 0 && (
+                    <p> Não há pets cadastrados ou disponíveis no momento! </p>
+                )}
+            </div>
+
         </section>
     )
 }
