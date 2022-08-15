4dd40c2dc5625f7dc723cabaf33cf3e6bc9e5b8b
Jheimys Bassani <jheimysbb@gmail.com>
2022-08-15 08:52:05 -0300

    Buscando pet via api

--- frontend/src/components/pages/pets/PetDetails.js
+++ frontend/src/components/pages/pets/PetDetails.js
@@ -11,9 +11,20 @@ import useFlashMessage from '../../../hooks/useFlashMessage'
 
 
 function PetDetails() {
+
+    const [pet, setPet] = useState({})
+    const {id} = useParams()
+    const {setFlashMessage} = useFlashMessage()
+    const [token] = useState(localStorage.getItem('token') || '')
+
+    useEffect(() => {
+        api.get(`/pets/${id}`).then((response) => {
+            setPet(response.data.pet)
+        })
+    },[id])
     
     return(
-        <h1>Página de Pet</h1>
+        <h1>{pet.name}</h1>
         
     ) 
         
